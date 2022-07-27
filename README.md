4dad5a7572514b5737ce2cd38b2d733707ea97de
Jon Giarla <jonathangiarla@gmail.com>
2022-07-27 05:26:54 -0400

    V 1.0 By default, the board starts on X's turn.  As players select spaces to play, the program changes turns as well as checks conditions after each turn to see if the game has been won or if there is a tie.  When the game is won or tied, the board is turned off and no longer playable until the game board is reset by the user.
    No AI functionality as of yet.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Tic-Tac-Toe
 A Tic-Tac-Toe program
+
+
+V 1.0 By default, the board starts on X's turn.  As players select spaces to play, the program changes turns as well as checks conditions after each turn to see if the game has been won or if there is a tie.  When the game is won or tied, the board is turned off and no longer playable until the game board is reset by the user.
+
+No AI functionality as of yet.
--- script.js
+++ script.js
@@ -2,10 +2,19 @@ document.getElementById("reset").onclick = function () { defaultGameBoard() }
 let player = "X"
 let boardArray = [...document.getElementsByClassName("board")]
 
-for (i = 0; i < boardArray.length; i++) {
-    console.log(boardArray.length)
-    boardArray[i].onclick = function () { pickSelection(this) }
+function startUpBoard() {
+    for (i = 0; i < boardArray.length; i++) {
+        boardArray[i].onclick = function () { pickSelection(this) }
+    }
 }
+
+function turnOffBoard() {
+    for (i = 0; i < boardArray.length; i++) {
+        boardArray[i].onclick = ''
+    }
+}
+
+startUpBoard()
 
 function pickSelection(element) {
     if (element.innerText == "") {
@@ -15,6 +24,7 @@ function pickSelection(element) {
             document.getElementById("playerTurn").innerText = `Player ${player}'s turn.`
         } else if (checkConditions() == 1) {
             document.getElementById("playerTurn").innerText = `Player ${player} wins!`
+            turnOffBoard()
             player = setPlayer(player)
         } else {
             document.getElementById("playerTurn").innerText = `You tied.`
@@ -34,6 +44,7 @@ function defaultGameBoard() {
     loadBoard().forEach(element => {
         element.innerText = ""
     })
+    startUpBoard()
     document.getElementById("playerTurn").innerText = `Player ${player}'s turn.`
 }
 
@@ -49,35 +60,14 @@ function checkConditions() {
         checkBoard.push(element.innerText)
     })
     console.log(checkBoard)
-    if (checkBoard[0] == player) {
-        console.log(`${player} is on 0`)
-        if (checkBoard[1] == player) {
-            if (checkBoard[2] == player) {
-                return 1
-            }
-        } else if (checkBoard[3] == player) {
-            if (checkBoard[6] == player) {
-                return 1
-            }
-        } else if (checkBoard[4] == player) {
-            if (checkBoard[8] == player) {
-                return 1
-            }
-        }
-    }
-    if ((checkBoard[1] == player) && (checkBoard[4] == player) && (checkBoard[7] == player)) {
-        return 1
-    }
-    if ((checkBoard[2] == player) && (checkBoard[5] == player) && (checkBoard[8] == player)) {
-        return 1
-    }
-    if ((checkBoard[4] == player) && (checkBoard[5] == player) && (checkBoard[6] == player)) {
-        return 1
-    }
-    if ((checkBoard[6] == player) && (checkBoard[7] == player) && (checkBoard[8] == player)) {
-        return 1
-    }
-    if ((checkBoard[2] == player) && (checkBoard[4] == player) && (checkBoard[6] == player)) {
+    if (((checkBoard[0] == player) && (checkBoard[1] == player) && (checkBoard[2] == player)) ||
+        ((checkBoard[0] == player) && (checkBoard[3] == player) && (checkBoard[6] == player)) ||
+        ((checkBoard[0] == player) && (checkBoard[4] == player) && (checkBoard[8] == player)) ||
+        ((checkBoard[1] == player) && (checkBoard[4] == player) && (checkBoard[7] == player)) ||
+        ((checkBoard[2] == player) && (checkBoard[5] == player) && (checkBoard[8] == player)) ||
+        ((checkBoard[3] == player) && (checkBoard[4] == player) && (checkBoard[5] == player)) ||
+        ((checkBoard[6] == player) && (checkBoard[7] == player) && (checkBoard[8] == player)) ||
+        ((checkBoard[2] == player) && (checkBoard[4] == player) && (checkBoard[6] == player))) {
         return 1
     }
     for (i = 0; i < checkBoard.length; i++) {
